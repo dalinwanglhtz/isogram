b549200c307f4e1dced293b4bcdddcdcc3e82fde
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-26 21:48:52 +1100

    2nd working cut

--- src/com/codewardev/Isogram.java
+++ src/com/codewardev/Isogram.java
@@ -1,10 +1,18 @@
 package com.codewardev;
 
+import java.util.Arrays;
+
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/java
+
 public class Isogram {
 
 	public static boolean isIsogram(String string) {
+		if(string.isEmpty()) return true;
+
+		String[] newString = Arrays.stream(string.split("")).map(String::toLowerCase).distinct().toArray(String[]::new);
+		if(newString.length != string.length()) return false;
 		
-		return false;
+		return true;
 	}
 
 }
