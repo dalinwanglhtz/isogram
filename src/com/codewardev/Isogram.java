eb220996db60affccb22fdb8311c2ec9d44b9f4c
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-27 21:31:42 +1100

    final version

--- src/com/codewardev/Isogram.java
+++ src/com/codewardev/Isogram.java
@@ -9,10 +9,10 @@ public class Isogram {
 	public static boolean isIsogram(String string) {
 		if(string.isEmpty()) return true;
 
-		String[] newString = Arrays.stream(string.split("")).map(String::toLowerCase).distinct().toArray(String[]::new);
-		if(newString.length != string.length()) return false;
-		
-		return true;
+		return Arrays.stream(string.split(""))
+				.map(String::toLowerCase)
+				.distinct()
+				.toArray(String[]::new).length != string.length()? false:true;
 	}
 
 }
